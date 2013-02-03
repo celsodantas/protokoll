43bc34298c79e8037009120611d3a03caa1b18db
Christian Naths <christiannaths@gmail.com>
2013-02-03 14:05:36 -0700

    updated implementation of 'start' option

--- lib/protokoll/counter.rb
+++ lib/protokoll/counter.rb
@@ -4,21 +4,21 @@ module Protokoll
   class Counter
     def self.next(object, options)
       element = Models::CustomAutoIncrement.find_or_create_by_model_name(object.class.to_s.underscore)
-      
-      element.counter = (0 + options[:start]) if outdated?(element, options)
+
+      element.counter = options[:start] if outdated?(element, options) || element.counter == 0
       element.counter += 1
 
       element.save!
-      
+
       Formater.new.format(element.counter, options)
     end
-    
-    private 
+
+    private
 
     def self.outdated?(record, options)
       Time.now.strftime(update_event(options)).to_i > record.created_at.strftime(update_event(options)).to_i
     end
-    
+
     def self.update_event(options)
       pattern = options[:pattern]
       event = String.new
