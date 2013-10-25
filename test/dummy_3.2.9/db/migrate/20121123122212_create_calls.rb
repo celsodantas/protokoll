c78699df4dc1044120df05d6d522bd26f183a585
Celso Dantas <celsodantas@gmail.com>
2013-10-24 19:46:22 -0500

    removing dummy_3.2.9 from project. If you want to test again a Rails app, create one and add the gem.

--- test/dummy_3.2.9/db/migrate/20121123122212_create_calls.rb
+++
@@ -1,9 +0,0 @@
-class CreateCalls < ActiveRecord::Migration
-  def change
-    create_table :calls do |t|
-      t.string :registry_number
-
-      t.timestamps
-    end
-  end
-end
--- test/dummy_3.2.9/test/unit/call_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CallTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
