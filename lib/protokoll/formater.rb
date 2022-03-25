9c6fb3b0d3df4d4bd61de6baa57cb205e67c9144
Celso Dantas <celsodantas@gmail.com>
2022-03-25 14:25:59 +0100

    trimming whitespaces

--- lib/protokoll/formater.rb
+++ lib/protokoll/formater.rb
@@ -1,32 +1,33 @@
+# frozen_string_literal: true
 
 module Protokoll
   class Formater
     def format(number, options)
       @options = options
-      
+
       build(number)
     end
-    
-    private 
-    
+
+    private
+
     # gets the next number.
     # it prepends the prefix + counter + sufix
     # ex:
-    #   "%Y####BANK" 
+    #   "%Y####BANK"
     #   %Y => prefix (year)
     #   #### => counter (starts with 0001)
     #   BANK => sufix
-    # 
+    #
     # if we are in 2011, the first model to be saved will get "20110001BANK"
     # the next model to be saved will get "20110002BANK", "20110003BANK"...
     #
     #   number => is the counter
-    #   
-    #   next_custom_number(1) 
+    #
+    #   next_custom_number(1)
     #   => "20110001BANK"
     def build(number)
-      prefix(@options[:pattern]).to_s + 
-      counter(@options[:pattern], number).to_s + 
+      prefix(@options[:pattern]).to_s +
+      counter(@options[:pattern], number).to_s +
       sufix(@options[:pattern]).to_s
     end
 
